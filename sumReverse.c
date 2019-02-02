ab16ad03a37860e286270e842020e8c8b0bb30af
Parker Gabel <parkerdgabel@email.arizona.edu>
2019-02-01 20:06:48 -0700

    Done

--- sumReverse.c
+++ sumReverse.c
@@ -1,15 +1,28 @@
+/*
+* File: sumReverse.c
+* Author: Parker Gabel
+* Purpose: Takes a positive integer and adds it to its reverse.
+*/
 #include <stdio.h>
 
+/*
+ * Reverses the given positive number
+ * params: num          positive integer
+ * returns:             The reversed number
+ */
 int reverseNumber(int num) {
     int reversed = 0, residue;
     while (num != 0) {
         residue = num % 10;
-        reversed = reversed*10 + residue;
+        reversed = reversed * 10 + residue;
         num /= 10;
     }
     return reversed;
 }
 
+/*
+ * Prints the reverse sum for all positive numbers in the input stream
+ */
 int main() {
     int inNum, retVal;
     while ((retVal = scanf("%d", &inNum)) > 0) {
